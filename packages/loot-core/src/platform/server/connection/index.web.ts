8c13f74354e034c39f8e490acf28837c385a131b
Leandro Menezes <lelemm@gmail.com>
2025-03-12 17:17:56 -0300

    removed unused code

--- packages/loot-core/src/platform/server/connection/index.web.ts
+++ packages/loot-core/src/platform/server/connection/index.web.ts
@@ -44,17 +44,6 @@ export const init: T.Init = function (serverChn, handlers) {
         return;
       }
 
-      else if (data.type === 'plugin-list') {
-        const processedData = {
-          message: 'Processed by Web Worker!',
-          timestamp: Date.now(),
-        };
-  
-        // Send back the response via MessagePort
-        e.ports[0].postMessage(processedData);
-      }
-
-
       if (msg.name === 'client-connected-to-backend') {
         // the client is indicating that it is connected to this backend. Stop attempting to connect
         console.info('Backend: Client connected');
